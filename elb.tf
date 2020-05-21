50584e0aa7f8566fce71ffa4ecb15019661f8ba1
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-21 10:54:01 -0300

    .

--- elb.tf
+++ elb.tf
@@ -11,12 +11,4 @@ resource "aws_elb" "elb" {
     lb_port = "80"
     lb_protocol = "tcp"
   }
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "TCP:80/check"
-    interval = 5
-  }
 }
