deda8536b97144a6bf449870b9a9e481f38e7fc9
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-24 00:28:26 -0300

    Ajustando sintaxe

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
-  default     = "sa-east-1"
+  default = "sa-east-1"
 }
 
 variable "vpc_cidr_block" {
-  default       = "10.10.10.0/24"
+  default = "10.10.10.0/24"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr_block
+  cidr_block = var.vpc_cidr_block
 
   tags = {
     Name = "Terraform VPC"
