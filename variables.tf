799dfccfdadc153b54253e6c72a74863379b5ea3
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-24 00:24:41 -0300

    configure provider and add vpc

--- variables.tf
+++ variables.tf
@@ -1,33 +1,7 @@
-variable "name" {
-  description = "The name for the resource"
-  default = "example"
-}
-
-variable "environment" {
-  description = "The env of the app"
-  default = "staging"
-}
-
 variable "region" {
   region     = "sa-east-1"
 }
 
-variable "access_key" {
-  access_key = "AKIAWNXSNZXXSVNHU45H"
-}
-
-variable "secret_key" {
-  secret_key = "DEj2oY4lGl7liE9kQxEP2DV2uCo+CAar2yqidPSW"
-}
-
 variable "vpc_cidr_block" {
   cidr_block       = "10.10.10.0/24"
-}
-
-variable "instance_type" {
-  intance_type = "t2.micro"
-}
-
-variable "image_id" {
-  image_id = "ami-03e1e4abf50e14ded"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "vpc-main" {
-  cidr_block       = "${var.vpc_cidr_block}"
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr_block
 
   tags = {
-    Name = "terraform"
+    Name = "Terraform VPC"
   }
 }
