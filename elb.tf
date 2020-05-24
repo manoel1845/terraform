8080e42df0f5766381514dc31b8af1a21effb3b4
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-24 01:50:48 -0300

    .

--- elb.tf
+++ elb.tf
@@ -2,21 +2,21 @@ resource "aws_lb" "elb" {
   name               = "elb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.sg-elb.id}"]
-  subnets            = ["${aws_subnet.subnet_a.elb.id"}, "${aws_subnet.subnet_c.elb.id"]
+  security_groups    = aws_security_group.sg-elb.id
+  subnets            = ["aws_subnet.subnet_a.elb.id", "aws_subnet.subnet_c.elb.id"]
 
   enable_deletion_protection = false
 
 }
 
 resource "aws_lb_listener" "elb_listener" {
-  load_balancer_arn = "${aws_lb.elb_listener.arn}"
+  load_balancer_arn = aws_lb.elb_listener.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.front_end.arn}"
+    target_group_arn = aws_lb_target_group.elb-tg.arn
   }
 }
 
