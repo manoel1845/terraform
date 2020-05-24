5ea74795b417944bd097db560177d130041263b1
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-24 01:48:07 -0300

    add ec2 and ajust elb

--- elb.tf
+++ elb.tf
@@ -1,40 +1,28 @@
+resource "aws_lb" "elb" {
+  name               = "elb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.sg-elb.id}"]
+  subnets            = ["${aws_subnet.subnet_a.elb.id"}, "${aws_subnet.subnet_c.elb.id"]
 
-resource "aws_elb" "elb" {
-  name = "elb_web"
-  security_groups = aws_security_group.sg-elb.id
-  cross_zone_load_balancing = true
-  subnets = ["aws_subnet.subnet_a.elb.id", "aws_subnet.subnet_c.elb.id"]
- # instances = ["${aws_instance.web-instance.id}"]
+  enable_deletion_protection = false
 
-  listener {
-    instance_port = "80"
-    instance_protocol = "tcp"
-    lb_port = "80"
-    lb_protocol = "tcp"
+}
+
+resource "aws_lb_listener" "elb_listener" {
+  load_balancer_arn = "${aws_lb.elb_listener.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.front_end.arn}"
   }
 }
 
 resource "aws_lb_target_group" "elb-tg" {
   name     = "elb-tg"
-  port     = 80
+  port     = "80"
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
-
-  health_check = {
-    path = "/"
-    health_threshold = 2
-  }
-}
-
-resource "aws_lb_listener" "elb_listener" {
-  load_balancer_arn = aws_lb.elb_listener.arn
-  port              = "80"
-  protocol          = "HTTP"
-  #ssl_policy        = "ELBSecurityPolicy-2016-08"
-  #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.elb-tg.arn
-  }
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,11 @@ variable "region" {
 variable "vpc_cidr_block" {
   default = "10.10.10.0/24"
 }
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "image_id" {
+  default = "ami-03e1e4abf50e14ded"
+}
