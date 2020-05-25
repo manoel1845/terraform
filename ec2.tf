1d60e846c7bb3298766504107a069f03f8fe217d
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-25 19:38:46 -0300

    add ec2

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web-instance" {
+  ami           = var.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg-web.id]
+  user_data = <<-EOF
+    #!/bin/bash
+    yum install -y httpd ; systemctl enable httpd
+    echo "Hello, World" > index.html
+    systemctl start httpd
+    EOF
+
+  tags = {
+    Name = "web"
+  }
+}
