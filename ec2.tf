1653d54ad387ce0ea1d4db8ab6211034f665c1fb
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-25 20:00:25 -0300

    add az and subnet to ec2

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "web-instance" {
   ami           = var.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.sg-web.id]
- /*
+  subnet_id = ["aws_sunet.subnet_a.id"]
+  availability_zone      = "${var.region}b"
+
   user_data = <<-EOF
     #!/bin/bash
     yum install -y httpd ; systemctl enable httpd
     echo "Hello, World" > index.html
     systemctl start httpd
     EOF
-*/
+
   tags = {
     Name = "web"
   }
