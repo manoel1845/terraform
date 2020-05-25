392cc21382c213b169fe4c9bf667ad8240587e1e
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-25 20:20:44 -0300

    .

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,7 @@
-resource "aws_security_group" "sg-web" {
-  name = "weg-sg"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.main.id
-  ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-      from_port = 0
-      to_port = 65535
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "web-instance" {
   ami           = var.image_id
   instance_type = var.instance_type
-  security_groups = aws_security_group.sg_web.id
+  security_groups = aws_security_group.sg-web.id
   subnet_id = aws_subnet.subnet_a.id
   availability_zone      = "${var.region}a"
 
