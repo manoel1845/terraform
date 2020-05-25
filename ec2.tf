26bbc10a7888a516567c0586724cd1a98e4026ee
manoel1845 <manoel_carlos16@hotmail.com>
2020-05-25 20:22:04 -0300

    ..

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-instance" {
   ami           = var.image_id
   instance_type = var.instance_type
-  security_groups = aws_security_group.sg-web.id
+  security_groups = [aws_security_group.sg-web.id]
   subnet_id = aws_subnet.subnet_a.id
   availability_zone      = "${var.region}a"
 
